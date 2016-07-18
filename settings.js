68a07ab7944ff200f32afbf85799040b8f1dd43c
yassir-ouali <yassir.oua@gmail.com>
2016-07-19 01:35:58 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,17 @@
+/**
+*
+*contient les infos de configuration 
+*nessessaire à l'acces à la bdd
+*Ainsi on pourra y acceder à partir de  n'importe quel
+*fichier source
+*
+*
+*
+*
+**/
+
+module.exports={
+	   url:'mongodb://127.0.0.1:27017/test',
+				collection:"profs" //nom de la collection
+				
+};
